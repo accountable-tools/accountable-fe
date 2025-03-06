3c3650e86eab5ea5f4893a38d459feae0102921d
Elizabeth PULLMAN <elizabethpullman@elizabeths-air.myfiosgateway.com>
2025-03-06 18:27:48 -0500

    chore: remove outdated accountable_tools folder and old Sidebar

--- accountable_tools/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen w-screen flex items-center justify-center bg-boneWhite text-slateCharcoal">
-      <h1 className="text-4xl font-bold">🚀 Accountable Tools on Vite!</h1>
-    </div>
-  );
-}
-
