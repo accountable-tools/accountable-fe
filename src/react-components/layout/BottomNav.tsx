c1b47eea14c865ec8ca13e1241b4ef53c3cde8e8
itzeitan <eitan@accountabletools.com>
2025-03-07 08:32:47 -0500

    Update BottomNav.tsx

--- src/react-components/layout/BottomNav.tsx
+++ src/react-components/layout/BottomNav.tsx
@@ -1,1 +1,9 @@
+const BottomNav = () => {
+  return (
+    <footer className="h-16 bg-gray-200 flex items-center justify-center transition-all duration-300">
+      <span>© 2025 Accountable Tools</span>
+    </footer>
+  );
+};
 
+export default BottomNav;
