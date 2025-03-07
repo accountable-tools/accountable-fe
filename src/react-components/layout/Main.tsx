a36254424969f8c46d7c931e87bdec49f6b4effd
itzeitan <eitan@accountabletools.com>
2025-03-07 08:32:24 -0500

    Update Main.tsx

--- src/react-components/layout/Main.tsx
+++ src/react-components/layout/Main.tsx
@@ -1,1 +1,11 @@
+import { ReactNode } from "react";
 
+interface MainProps {
+  children: ReactNode;
+}
+
+const Main = ({ children }: MainProps) => {
+  return <main className="flex-1 p-6 transition-all duration-300">{children}</main>;
+};
+
+export default Main;
