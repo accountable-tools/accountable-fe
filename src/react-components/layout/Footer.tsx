8a30c0d8da2d831fbaea16842a60a74304f8e229
itzeitan <eitan@accountabletools.com>
2025-03-08 18:54:34 -0500

    Update Footer.tsx

--- src/react-components/layout/Footer.tsx
+++ src/react-components/layout/Footer.tsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="h-16 bg-gray-200 flex items-center justify-center">
-      {/* TODO: Footer content */}
-      <span>&copy; 2025 Accountable Tools</span>
+    <footer className="h-16 bg-gray-200 flex items-center justify-center transition-all duration-300">
+      <span>© 2025 Accountable Tools</span>
     </footer>
   );
 }
