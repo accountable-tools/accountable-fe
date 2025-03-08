57edad474d4c3a80659d152e6722889770d1dd67
itzeitan <eitan@accountabletools.com>
2025-03-08 18:44:27 -0500

    Update types.ts

--- src/react-components/types.ts
+++ src/react-components/types.ts
@@ -1,1 +1,8 @@
-// Shared types for the react-components
+// src/react-components/types.ts
+
+export type ExampleType = {
+  id: number;
+  name: string;
+};
+
+export const exampleConstant = "This is a sample constant";
