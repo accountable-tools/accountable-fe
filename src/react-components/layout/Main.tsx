11987f919326efd5b5830fc38d1d45729711dfea
itzeitan <eitan@accountabletools.com>
2025-03-08 18:59:54 -0500

    Update Main.tsx

--- src/react-components/layout/Main.tsx
+++ src/react-components/layout/Main.tsx
@@ -1,11 +1,30 @@
-import { ReactNode } from "react";
+import React from "react";
+import TopNav from "@/react-components/layout/TopNav";
+import Body from "@/react-components/layout/Body";
+import Footer from "@/react-components/layout/Footer";
 
-interface MainProps {
-  children: ReactNode;
+export default function Main({
+  children,
+  isCollapsed,
+}: {
+  children: React.ReactNode;
+  isCollapsed: boolean;
+}) {
+  return (
+    <div
+      className="flex flex-col min-h-screen transition-all duration-300"
+      style={{
+        width: isCollapsed ? "calc(100% - 4rem)" : "calc(100% - 16rem)",
+      }}
+    >
+      {/* Top Navigation */}
+      <TopNav />
+
+      {/* Main Content Area */}
+      <Body>{children}</Body>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
-
-const Main = ({ children }: MainProps) => {
-  return <main className="flex-1 p-6 transition-all duration-300">{children}</main>;
-};
-
-export default Main;
