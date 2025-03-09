4d9532dd573e4d808f98cf0e72e850d80865137b
itzeitan <eitan@accountabletools.com>
2025-03-08 23:18:49 -0500

    Update TopNav.tsx

--- src/react-components/layout/TopNav.tsx
+++ src/react-components/layout/TopNav.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-
 export default function TopNav() {
   return (
-    <nav className="h-16 bg-white shadow-md flex items-center px-4">
-      {/* TODO: Nav content */}
+    <nav className="h-16 shadow-md flex items-center px-4">
       <h1 className="text-lg font-semibold">TopNav</h1>
     </nav>
   );
