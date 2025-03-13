a4762434873f94eabc506f58ebfc6c826ed18e29
itzeitan <eitan@accountabletools.com>
2025-03-12 21:16:15 -0400

    Create TopNav.tsx

---
+++ app/layout/TopNav.tsx
@@ -0,0 +1,7 @@
+export default function TopNav() {
+  return (
+    <nav className="h-16 shadow-md flex items-center px-4">
+      <h1 className="text-lg font-semibold">TopNav</h1>
+    </nav>
+  );
+}
