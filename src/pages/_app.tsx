621463abea4b6aa3ed0323998bb00417b10396e0
itzeitan <eitan@accountabletools.com>
2025-03-12 21:38:07 -0400

    Delete src/pages/_app.tsx

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from "next/app";
-import "@/styles/globals.css"; // ✅ Move global styles here
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
