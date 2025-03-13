416d60737d9efed125729bbd63109c5f693ec9fc
itzeitan <eitan@accountabletools.com>
2025-03-12 21:47:12 -0400

    Delete app/layout/TopNav.tsx

--- app/layout/TopNav.tsx
+++
@@ -1,7 +0,0 @@
-export default function TopNav() {
-  return (
-    <nav className="h-16 shadow-md flex items-center px-4">
-      <h1 className="text-lg font-semibold">TopNav</h1>
-    </nav>
-  );
-}
