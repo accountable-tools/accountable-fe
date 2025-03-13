af5907e3faf7ad518237a48d29f247233214efa4
itzeitan <eitan@accountabletools.com>
2025-03-12 21:15:38 -0400

    Create Main.tsx

---
+++ app/layout/Main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import TopNav from "@/react-components/layout/TopNav";
+import Body from "@/react-components/layout/Body";
+import Footer from "@/react-components/layout/Footer";
+
+export default function Main({
+  children,
+  isCollapsed,
+}: {
+  children: React.ReactNode;
+  isCollapsed: boolean;
+}) {
+  return (
+    <div
+      className="flex flex-col min-h-screen bg-cream transition-all duration-300" // <-- bg-cream moved here
+      style={{
+        width: isCollapsed ? "calc(100% - 4rem)" : "calc(100% - 16rem)",
+      }}
+    >
+      {/* Top Navigation */}
+      <TopNav />
+
+      {/* Main Content Area */}
+      <Body>{children}</Body>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
