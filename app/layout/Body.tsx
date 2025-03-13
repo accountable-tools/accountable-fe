eff2e4f6c4b0864b2e22ff21904d74d617e85066
itzeitan <eitan@accountabletools.com>
2025-03-12 21:46:07 -0400

    Delete app/layout/Body.tsx

--- app/layout/Body.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Body({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex-1 p-6 transition-all duration-300">
-      {children}
-    </div>
-  );
-}
